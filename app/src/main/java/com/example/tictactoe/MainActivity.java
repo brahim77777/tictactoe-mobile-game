2c502a91b86b39bcfac8b35d12e1d91f565b8e33
brahim <brahim@brahims-MacBook-Pro.local>
2025-04-07 10:59:54 +0100

    adding the logic of who won , still needs some better ui

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -14,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView img[] = new ImageView[9];
     private int id[] = new int[9];
+    private int record[] = {-1,-1,-1,-1,-1,-1,-1,-1,-1};
+
+    public void printWinner(int i ){
+        if(i == 0 )
+            findViewById(R.id.rs).setBackgroundColor(Color.RED);
+            else{
+
+            findViewById(R.id.rs).setBackgroundColor(Color.GREEN);
+
+        }
+    }
     int i,click;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +45,71 @@ public class MainActivity extends AppCompatActivity {
             img[i] = findViewById(id[i]);
             final int j= i;
             img[i].setOnClickListener(v -> {
-                if (click % 2 == 0)
-                img[j].setImageResource(R.drawable.x);
-                else
+
+
+                if (click % 2 == 0){
+                    img[j].setImageResource(R.drawable.x);
+                    record[j] = 1;}
+                else {
                     img[j].setImageResource(R.drawable.circle);
+                    record[j] = 0;
+                }
                 click++;
+
+
+
+
+
+                for (int i = 0  ; i < 8; i+=3)
+                        if(record[i] == record[i+1] && record[i] == record[i+2] ){
+                            if (record[i] == 1){
+                                // x win
+                                printWinner(record[i]);
+                            } else if (record[i] == 0) {
+                                // o win
+                                printWinner(record[i]);
+
+                            }
+                        }
+                for (int i = 0 ;i < 3 ; i++ )
+                    if(record[i] == record[i+3] && record[i] == record[i+6] ){
+                        if (record[i] == 1){
+                            // x win
+                            printWinner(record[i]);
+
+                        } else if (record[i] == 0) {
+                            // o win
+                            printWinner(record[i]);
+
+                        }
+                    }
+
+                if(record[0] == record[4] && record[0] == record[8]) {
+                    if (record[0] == 1) {
+                        // x win
+                        printWinner(record[0]);
+
+                    } else if (record[0] == 0) {
+                        // o win
+                        printWinner(record[0]);
+
+                    }
+                }
+                if(record[2] == record[4] && record[2] == record[6]) {
+                    if (record[2] == 1) {
+                        // x win
+                        printWinner(record[2]);
+
+                    } else if (record[2] == 0) {
+                        // o win
+                        printWinner(record[2]);
+
+                    }
+                }
+
+
+
+
             });
         }
 
